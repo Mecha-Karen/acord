d44bd4dfaeccb50e9e80cfeebdea888f8faf110e
1olipop <mohammedrayyan112233@gmail.com>
2021-11-15 16:05:42 +0530

    Modified the User Model according to the docs

--- acord/models/user.py
+++ acord/models/user.py
@@ -1,14 +1,21 @@
 import pydantic
+from typing import Optional
 
 
+# https://discord.com/developers/docs/resources/user#get-user
 class User(pydantic.BaseModel):
-    verified: bool
-    username: str
-    mfa_enabled: bool
-    id: int
-    flags: int
-    discriminator: int
-    bot: str
-    avatar: str
-
-    email: str = None
+    id: int # Change this to ~acord.snowflakes.UserSnowflake~ later
+    username: str # the user's username, not unique across the platform
+    discriminator: str # the user's 4-digit discord-tag
+    avatar: Optional[str] # the user's avatar hash
+    bot: bool # whether the user belongs to an OAuth2 application
+    system: bool # 	whether the user is an Official Discord System user (part of the urgent message system)
+    mfa_enabled: bool # whether the user has two factor enabled on their account
+    banner: Optional[str] # the user's banner hash
+    accent_color: Optional[int] # the user's banner color encoded as an integer representation of hexadecimal color code
+    locale: str # the user's chosen language option
+    verified: bool # whether the email on this account has been verified
+    email: Optional[str] # the user's email
+    flags: Optional[int] # the flags on a user's account
+    premium_type: Optional[int] # the type of Nitro subscription on a user's account
+    public_flags: Optional[int] # the public flags on a user's account
