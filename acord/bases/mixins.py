e783d428b7620bb01edf36ace86275f0c828b0d1
Seniatical <isaja7710@gmail.com>
2021-11-17 18:24:04 +0000

    Typehint for eq and ne dunders

--- acord/bases/mixins.py
+++ acord/bases/mixins.py
@@ -1,12 +1,15 @@
+from typing import TypeVar, Type
+
+H = TypeVar('H', 'Hashable')
 class Hashable:
     __slots__ = ()
 
     id: int
 
-    def __eq__(self, O) -> bool:
+    def __eq__(self, O: Type[H]) -> bool:
         return self.id == O.id
 
-    def __ne__(self, O) -> bool:
+    def __ne__(self, O: Type[H]) -> bool:
         return self.id != O.id
 
     def __hash__(self) -> int:
