7b1ee532105562b159ca8b9ee19a0b462f44a373
Seniatical <isaja7710@gmail.com>
2021-11-17 19:48:50 +0000

    Basic user object

--- acord/models/user.py
+++ acord/models/user.py
@@ -1,14 +1,35 @@
+from __future__ import annotations
+
 import pydantic
+from acord.bases import Hashable
+from typing import Any, Optional
 
 
-class User(pydantic.BaseModel):
-    verified: bool
-    username: str
-    mfa_enabled: bool
-    id: int
-    flags: int
-    discriminator: int
-    bot: str
-    avatar: str
+class User(pydantic.BaseModel, Hashable):
+    id: int                     # TODO: Change this to ~acord.types.UserSnowflake~ later
+    username: str               # the user's username, not unique across the platform
+    discriminator: str          # the user's 4-digit discord-tag
+    avatar: Optional[str]       # the user's avatar hash
+    bot: bool                   # whether the user belongs to an OAuth2 application
+    system: Optional[str]       # whether the user is an Official Discord System user (part of the urgent message system)
+    mfa_enabled: bool           # whether the user has two factor enabled on their account
+    banner: Optional[str]       # the user's banner hash
+    accent_color: Optional[int] # the user's banner color encoded as an integer representation of hexadecimal color code
+    locale: Optional[str]       # the user's chosen language option
+    verified: bool              # whether the email on this account has been verified
+    flags: Optional[int]        # the flags on a user's account
+    premium_type: Optional[int] # the type of Nitro subscription on a user's account
+    public_flags: Optional[int] # the public flags on a user's account
+    email: Optional[str]        # the user's email, can be None as bots cannot have an email
 
-    email: str = None
+    @pydantic.validator('avatar')
+    def _validateEmail(cls, av: str, **kwargs) -> str:
+        id = kwargs['values']['id']
+
+        return f'https://cdn.discordapp.com/avatars/{id}/{av}.png'
+
+    def __init__(self, conn, **data: Any) -> None:
+        # Over ride pydantic setattr
+        self.__dict__['_conn'] = conn
+
+        super().__init__(**data)
