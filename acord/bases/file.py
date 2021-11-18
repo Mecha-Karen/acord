e9fbf617b7d215207af734b038fcdf3f171dad2b
Seniatical <isaja7710@gmail.com>
2021-11-18 19:29:02 +0000

    Change to pydantic object

--- acord/bases/file.py
+++ acord/bases/file.py
@@ -1,29 +1,40 @@
 import os
 import io
-from typing import Optional, Union
+from typing import Optional, Union, Type
+import pydantic
 
 
-class File:
-    def __init__(
-        self, *,
-        fp: Union[str, os.PathLike, io.BufferedIOBase],
-        filename: Optional[str] = None,
-        spoiler: Optional[bool] = False
-    ):
+class File(pydantic.BaseModel):
+    fp: Union[str, Type[os.PathLike], Type[io.BufferedIOBase]]
+    position: Optional[int] = 0
+    filename: Optional[str]
+    spoiler: Optional[bool] = False
+    is_closed: Optional[bool] = False
+
+    @pydantic.validator('fp')
+    def _validate_fp(cls, fp, **kwargs):
         if not isinstance(fp, io.BufferedIOBase):
-            self._fp = open(fp, 'rb')
-            self._positon = 0
+            fp = open(fp, 'rb')
         else:
-            self._fp = fp
-            self._positon = fp.tell()
+            kwargs['values']['position'] = fp.tell()
         
-        self.filename = filename
+        return fp
+
+    @pydantic.validator('spoiler')
+    def _validate_spoiler(cls, spoiler, **kwargs):
+        filename = kwargs['values']['filename']
         
-        if spoiler and self.filename is not None and not self.filename.startswith('SPOILER_'):
-            self.filename = "SPOILER_" + self.filename
+        if filename and filename is not None and not filename.startswith('SPOILER_'):
+            filename = "SPOILER_" + filename
+            kwargs['values']['filename'] = filename
 
-    def reset(self):
-        self._fp.seek(0)
+        return spoiler
 
-    def close(self):
-        self._fp.close()
+    def reset(self, seek: Optional[bool] = False, position: Optional[int] = 0) -> None:
+        if not seek:
+            return
+        self.fp.seek(position)
+
+    def close(self) -> None:
+        self.fp.close()
+        self.is_closed = True
