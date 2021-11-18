8bb690be560766b85e843bdf834308d6edbe2849
Seniatical <isaja7710@gmail.com>
2021-11-18 19:35:23 +0000

    Add depreciated error

--- acord/errors.py
+++ acord/errors.py
@@ -3,7 +3,6 @@ class BaseResponseException(Exception):
     def __new__(cls, *args, **kwargs):
         return super(BaseResponseException, cls).__new__(BaseResponseException)
 
-
 class HTTPException(BaseResponseException):
     def __init__(self, code, message):
         super().__init__(f'Status {code}: {message}')
@@ -11,6 +10,8 @@ class HTTPException(BaseResponseException):
 class GatewayConnectionRefused(BaseResponseException):
     """ Raised when connecting to gateway fails """
 
+class APIObjectDepreciated(BaseResponseException):
+    """ Raised when a certain item in the api has been depreciated """
 
 class CannotOverideTokenWarning(Warning):
     """ Warned when cannot use provided token due to binded token present """
