95e111c75d9ebaa7a08481aee9f3cd6f12979d77
Seniatical <isaja7710@gmail.com>
2021-11-19 16:49:25 +0000

    Add cacheit function

--- acord/core/helpers.py
+++ acord/core/helpers.py
@@ -1,6 +1,8 @@
 """
 All version related info for connecting to the gateway.
 """
+from functools import wraps
+from ..core.signals.gateway import INTERNAL_STORAGE
 import yarl
 from typing import Optional, Literal
 
@@ -25,6 +27,27 @@ def buildURL(*paths, **parameters) -> str:
     return yarl.URL(URI)
 
 
+# Should be used for caching response objects which return on call
+def cacheit(section: str, store = INTERNAL_STORAGE, maxItems = 1000):
+    def inner(func):
+        if section not in store:
+            store[section] = set()
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            res = func(*args, **kwargs)
+            
+            if len(store[section]) >= maxItems:
+                store[section] = store[section][:maxItems - 1]
+
+            store[section].update([res])
+
+            return res
+        return inner
+
+    return inner
+
+
 class Route(object):
     """ Simple object representing a route """
     def __init__(self, method: str = "GET", *paths, path="/", **parameters):
@@ -37,9 +60,9 @@ class Route(object):
         self.method = method
         self.url = buildURL(*paths, **parameters)
 
-        self.channel_id: Optional[...] = parameters.get('channel_id')
-        self.guild_id: Optional[...] = parameters.get('guild_id')
-        self.webhook_id: Optional[...] = parameters.get('webhook_id')
+        self.channel_id: Optional[int] = parameters.get('channel_id')
+        self.guild_id: Optional[int] = parameters.get('guild_id')
+        self.webhook_id: Optional[int] = parameters.get('webhook_id')
         self.webhook_token: Optional[str] = parameters.get('webhook_token')
 
     @property
