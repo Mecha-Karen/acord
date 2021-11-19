b5dfac259fe4b17ac42964bb6909aeb3c445fe7a
Seniatical <isaja7710@gmail.com>
2021-11-19 18:40:09 +0000

    Add forbidden for 403's

--- acord/errors.py
+++ acord/errors.py
@@ -13,5 +13,8 @@ class GatewayConnectionRefused(BaseResponseException):
 class APIObjectDepreciated(BaseResponseException):
     """ Raised when a certain item in the api has been depreciated """
 
+class Forbidden(BaseResponseException):
+    """ Raised when request returns 403 indicating that you don't have sufficient permissions """
+
 class CannotOverideTokenWarning(Warning):
     """ Warned when cannot use provided token due to binded token present """
