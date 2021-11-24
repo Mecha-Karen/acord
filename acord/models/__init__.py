fd8faeec0eb1314ebcbaff9f379a73b20dfd445a
Seniatical <isaja7710@gmail.com>
2021-11-24 19:37:34 +0000

    Add snowflake type

--- acord/models/__init__.py
+++ acord/models/__init__.py
@@ -1,5 +1,11 @@
+from typing import TypeVar, Union
+
+Snowflake = TypeVar("Snowflake", bound=int)
+
+# Simple snowflake object
+
 from .user import User
-from .message import Message
 from .emoji import Emoji
+from .message import Message
 
 Message.update_forward_refs()
