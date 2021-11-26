490f5cc96caffea50dc3a69b34ad3f4c7d203414
Seniatical <isaja7710@gmail.com>
2021-11-26 18:16:09 +0000

    Import channel flags

--- acord/bases/__init__.py
+++ acord/bases/__init__.py
@@ -1,5 +1,10 @@
 from .flags.intents import Intents
 from .flags.user import UserFlags
+from .flags.channels import ChannelTypes, VoiceQuality
 
 from .file import File
-from .mixins import Hashable
+from .mixins import Hashable, _C, T, H
+
+
+class MISSING(object):
+    """ Identifer class, used to represent a value which doesn't exist, or should be set to 0 """
