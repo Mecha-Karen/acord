39b044c96f8f2c393406ab1eb10dd51e6f41bc14
Seniatical <isaja7710@gmail.com>
2021-12-06 20:38:39 +0000

    Patch Fix: remove auto method

--- acord/bases/mentions.py
+++ acord/bases/mentions.py
@@ -1,15 +1,14 @@
 # Smaller API objects that dont need seperate files
 from __future__ import annotations
-from typing import List, Optional, Union
+from typing import List, Optional
 
 import pydantic
-from acord.models import User, Message
 
 
 class AllowedMentions(pydantic.BaseModel):
     roles: Optional[List[int]]
     """ List of roles to mention """
-    users: Optional[List[Union[int, User]]]
+    users: Optional[List[int]]
     """ List of users to mention """
     replied_user: Optional[bool] = False
     """ Whether to mention to user being replied """
@@ -17,8 +16,6 @@ class AllowedMentions(pydantic.BaseModel):
     # Other relevant stuff
     everyone: Optional[bool] = False
     """ Whether to allow @everyone / @here pings """
-    auto: Optional[bool] = False
-    """ Whether to find up a 100 roles and user mentions when they are set to False """
     deny_all: Optional[bool] = False
     """ Simply reject every single mention on message """
 
@@ -47,16 +44,6 @@ class AllowedMentions(pydantic.BaseModel):
             mentions.append('everyone')
         return mentions
 
-    def run_auto(self, message: Message) -> None:
-        if not self.auto:
-            return
-
-        user_mentions = message.mentions[:100]
-        role_mentions = message.mention_roles[:100]
-
-        self.users = list(map(lambda x: x.id, user_mentions))
-        self.roles = list(map(lambda x: x.id, role_mentions))
-
     def dict(self, **kwargs):
         # :meta private:
         # Override pydantic to return proper AllowedMention structure
@@ -69,8 +56,6 @@ class AllowedMentions(pydantic.BaseModel):
             }
 
         parsed = super(AllowedMentions, self).dict(**kwargs)
-        
-        parsed.pop('auto')
 
         parsed.pop('everyone')
         parsed.pop('deny_all')
