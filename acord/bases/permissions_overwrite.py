7358c202e184a770d58448c621d1065e856c668d
Seniatical <isaja7710@gmail.com>
2021-12-11 16:49:35 +0000

    Allow role or members to be used

---
+++ acord/bases/permissions_overwrite.py
@@ -0,0 +1,19 @@
+import pydantic
+from acord.models import Snowflake
+from typing import Literal
+from .flags.permissions import Permissions
+
+
+class PermissionsOverwrite(pydantic.BaseModel):
+    id: Snowflake
+    type: Literal[0, 1]
+    allow: Permissions
+    deny: Permissions
+
+    @pydantic.validator("type", pre=True)
+    def _str_to_literal(cls, value):
+        if value == "role":
+            return 0
+        elif value == "member":
+            return 1
+        return value
