ee1c887ee32a3eb038ac818304f8e2a47719ca7e
Seniatical <isaja7710@gmail.com>
2021-12-12 15:39:04 +0000

    basic utility methods

---
+++ acord/utils.py
@@ -0,0 +1,15 @@
+# Some helper objects used in the lib
+
+from typing import Any
+
+
+def _payload_dict_to_json(base, **keys) -> Any:
+    # Converts kwargs to base excluding all keys
+    base = base(**keys)
+    to_exclude = list()
+    
+    for key, value in base.dict().items():
+        if value is None and key not in keys:
+            to_exclude.append(key)
+    
+    return base.json(exclude=set(to_exclude))
