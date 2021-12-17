774b9773e5102c1f37b27eb71003521f667d7c86
Seniatical <isaja7710@gmail.com>
2021-12-17 16:33:58 +0000

    complete thread object

--- acord/models/channels/thread.py
+++ acord/models/channels/thread.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
-from typing import Any, Optional
+from typing import Any, Dict, Iterator, List, Literal, Optional, Union
 
 import pydantic
 import datetime
 
-from acord.bases import Hashable
 from acord.core.abc import Route
 from acord.models import Snowflake, Member
 
@@ -21,6 +20,9 @@ class ThreadMeta(pydantic.BaseModel):
 
 class ThreadMember(pydantic.BaseModel):
     id: Snowflake
+    user_id: Snowflake
+    join_timestamp: datetime.datetime
+    flags: int
 
 
 class Thread(Channel):
@@ -49,9 +51,114 @@ class Thread(Channel):
     thread_metadata: ThreadMeta
     """ Additional data about thread """
 
+    members: Dict[Snowflake, ThreadMember] = dict()
+    """ Mapping of members in thread, fills with each fetch """
+
     async def join(self) -> None:
         """|coro|
 
         Joins current thread
         """
-        await self.conn.request(Route("PUT", path=f'/channels/{self.parent_id}/thread-members/@me'))
+        await self.add_member(member="@me")
+
+    async def leave(self) -> None:
+        """|coro|
+
+        Leaves current thread
+        """
+        await self.remove_member(member="@me")
+
+    async def add_member(self, *, member: Union[Member, Snowflake]) -> None:
+        """|coro|
+        
+        Adds a member to the thread
+
+        Parameters
+        ----------
+        member: Union[:class:`Member`, :class:`Snowflake`]
+        """
+        if member != "@me":
+            member = member.id
+
+        await self.conn.request(Route("PUT", path=f'/channels/{self.id}/thread-members/{member}'))
+
+    async def remove_member(self, *, member: Union[Member, Snowflake]) -> None:
+        """|coro|
+
+        Removes a member from the thread
+
+        Parameters
+        ----------
+        member: Union[:class:`Member`, :class:`Snowflake`]
+        """
+        if isinstance(member, Union[Member, Snowflake]):
+            member = member.id
+
+        await self.conn.request(Route("DELETE", path=f'/channels/{self.id}/thread-members/{member}'))
+        self.members.pop(member, None)
+
+    async def fetch_member(
+        self, 
+        *, 
+        member: Union[Member, Snowflake],
+        as_guild_member: bool = False
+        ) -> Optional[Union[ThreadMember, Member]]:
+        """|coro|
+
+        Fetches member from thread
+
+        Parameters
+        ----------
+        member: Union[:class:`Member`, :class:`Snowflake`]
+            Member to fetch
+        as_guild_member: :class:`bool`
+            whether to return the fetched member as a :class:`ThreadMember` object or :class:`Member`
+        """
+        if isinstance(member, Member):
+            member = member.id
+
+        r = await self.conn.request(
+            Route("GET", path=f'/channels/{self.id}/thread-members/{member}')
+        )
+
+        tmember = ThreadMember(**(await r.json()))
+        self.members.update({member.user_id: tmember})
+
+        if not as_guild_member:
+            member = tmember
+        else:
+            id = (await r.json())['user_id']
+            guild = self.conn.client.get_guild(self.guild_id)
+            member = guild.get_member(id)
+
+        return member
+
+    async def fetch_members(
+        self, 
+        *, 
+        as_guild_member: bool = False
+        ) -> Iterator[Union[Member, ThreadMember]]:
+        """|coro|
+
+        Fetches all members in the thread
+
+        Parameters
+        ----------
+        as_guild_member: :class:`bool`
+            Whether to return the thread members as :class:`ThreadMember` or :class:`Member`
+        """
+        r = await self.conn.request(
+            Route("GET", path=f'/channels/{self.id}/thread-members')
+        )
+        members = await r.json()
+        guild = self.conn.client.get_guild(self.guild_id)
+
+        for member in members:
+            nmember = ThreadMember(**member)
+            self.members.update({nmember.user_id: nmember})
+
+            if as_guild_member:
+                id = member['user_id']
+                yield guild.get_member(id)
+            else:
+                yield nmember
