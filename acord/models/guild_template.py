516aa78cb99cec13abbc15eb3fc4ed9bd9dfacd2
Seniatical <isaja7710@gmail.com>
2021-12-18 20:13:59 +0000

    Fix docstrings

--- acord/models/guild_template.py
+++ acord/models/guild_template.py
@@ -10,24 +10,24 @@ class GuildTemplate(pydantic.BaseModel):
     conn: Any
 
     code: str
-    # the template code (unique ID)
+    """ the template code (unique ID) """
     name: str
-    # template name
+    """ template name """
     description: str
-    # the description for the template
+    """ the description for the template """
     usage_count: int
-    # number of times this template has been used
+    """ number of times this template has been used """
     creator_id: Snowflake
-    # the ID of the user who created the template
+    """ the ID of the user who created the template """
     creator: User
-    # the user who created the template
+    """ the user who created the template """
     created_at: str
-    # when this template was created
+    """ when this template was created """
     updated_at: str
-    # when this template was last synced to the source guild
+    """ when this template was last synced to the source guild """
     source_guild_id: Snowflake
-    # the ID of the guild this template is based on
+    """ the ID of the guild this template is based on """
     serialized_source_guild: Guild
-    # the guild snapshot this template contains
+    """ the guild snapshot this template contains """
     is_dirty: Optional[bool]
-    # whether the template has unsynced changes
+    """ whether the template has unsynced changes """
