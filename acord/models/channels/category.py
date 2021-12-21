9998349a86d89264b28d3f20e2510ef79f53c84a
Seniatical <isaja7710@gmail.com>
2021-12-21 16:44:52 +0000

    Make NSFW optional

--- acord/models/channels/category.py
+++ acord/models/channels/category.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 
 from acord.bases import PermissionsOverwrite
 from acord.models import Snowflake
@@ -8,11 +8,11 @@ from .base import Channel
 
 
 class CategoryChannel(Channel):
-    permission_overwrites: List[PermissionsOverwrite]
+    permission_overwrites: Optional[List[PermissionsOverwrite]] = list()
     """ List of permissions for category """
     name: str
     """ Name of category """
-    nsfw: bool
+    nsfw: Optional[bool] = False
     """ Whether the category is marked as NSFW """
     position: int
     """ Position of category """
