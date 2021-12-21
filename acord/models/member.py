9f974b2c5169aafc249e315a86ac2887bc472ef3
Seniatical <isaja7710@gmail.com>
2021-12-21 19:56:17 +0000

    Add role management for members

--- acord/models/member.py
+++ acord/models/member.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
-from os import path
-
 from typing import Any, List, Optional
 import pydantic
 import datetime
+
 from acord.bases import Hashable
 from acord.core.abc import Route
-from acord.models import Snowflake
+from acord.models import Snowflake, Role
+from acord.payloads import MemberEditPayload
+from acord.utils import _payload_dict_to_json
 
 from .user import User
 
@@ -80,3 +81,141 @@ class Member(pydantic.BaseModel, Hashable):
             headers=headers,
             data=data
         )
+
+    async def kick(self, *, reason: str = None) -> None:
+        """|coro|
+
+        Kicks this member from the guild
+
+        Parameters
+        ----------
+        reason: :class:`str`
+            Reason for kicking member
+        """
+        headers = dict()
+
+        if reason:
+            headers.update({'X-Audit-Log-Reason': reason})
+
+        await self.conn.request(
+            Route("DELETE", path=f"/guilds/{self.guild_id}/members/{self.user.id}"),
+            headers=headers
+        )
+
+    async def edit(self, *, reason: str = None, **data) -> Member:
+        """|coro|
+
+        Modifies current member
+
+        Parameters
+        ----------
+        nick: :class:`str`
+            New nickname for user
+        roles: :class:`str`
+            New roles for members,
+            roles will be updated exactly as provided.
+            If you wish to keep existing roles, use :meth:`Member.add_role`.
+        mute: :class:`bool`
+            whether the user is muted in voice channels. 
+            Raises BadRequest if user is not in a VC.
+        deaf: :class:`bool`
+            whether the user is deafened in voice channels. 
+            Raises BadRequest if user is not in a VC.
+        channel_id: :class:`Snowflake`
+            id of channel to move user to (if they are connected to voice).
+        communication_disabled_until: :obj:`py:datetime.datetime`
+            User communication timeout.
+            If set to ``None``, removes timeout.
+        """
+        payload = _payload_dict_to_json(MemberEditPayload, **data)
+        headers = dict({'Content-Type': 'application/json'})
+
+        if reason:
+            headers.update({'X-Audit-Log-Reason': reason})
+
+        r = await self.conn.request(
+            Route("PATCH", path=f"/guilds/{self.guild_id}/members/{self.user.id}"),
+            headers=headers,
+            data=payload
+        )
+
+        member = Member(guild_id=self.guild_id, conn=self.conn, **(await r.json()))
+        guild = self.conn.client.get_guild(member.guild_id)
+        guild.members.update({member.id: member})
+
+        return member
+
+    async def add_role(self, role: Role, *, reason: str = None) -> None:
+        """|coro|
+
+        Adds a single role to user, for more roles consider using:
+
+        * :meth:`Member.edit`
+        * :meth:`Member.add_roles`
+
+        Parameters
+        ----------
+        role: :class:`Role`
+            Role to add to user
+        reason: :class:`str`
+            Reason for adding role
+        """
+        headers = dict()
+
+        if reason:
+            headers.update({'X-Audit-Log-Reason': reason})
+
+        await self.conn.request(
+            Route("PUT", path=f"/guilds/{self.guild_id}/members/{self.user.id}/roles/{role.id}"),
+            headers=headers
+        )
+
+        if role not in self.roles:
+            self.roles.append(role)
+
+    async def add_roles(self, *roles, reason: str = None) -> Member:
+        """|coro|
+
+        Adds many roles to the user in one go,
+        utility method for :meth:`Member.edit`.
+
+        Parameters
+        ----------
+        *roles: :class:`Role`
+            Roles to be updated, 
+            must be provided individually as args!
+        reason: :class:`str`
+            Reason for adding roles
+        """
+        n_roles = self.roles
+
+        for role in roles:
+            if role not in n_roles:
+                n_roles.append(role)
+
+        return await self.edit(roles=n_roles, reason=reason)
+
+    async def remove_role(self, role: Role, *, reason: str = None) -> None:
+        """|coro|
+
+        Remove a role from member
+
+        Parameters
+        ----------
+        role: :class:`Role`
+            Role to remove
+        reason: :class:`str`
+            Reason for removing role
+        """
+        headers = dict()
+
+        if reason:
+            headers.update({'X-Audit-Log-Reason': reason})
+
+        await self.conn.request(
+            Route("DELETE", path=f"/guilds/{self.guild_id}/members/{self.user.id}/roles/{role.id}"),
+            headers=headers
+        )
+
+        if role in self.roles:
+            self.roles.remove(role)
