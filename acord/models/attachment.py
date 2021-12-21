e22e815b92e4a55ef4b3c06d84320656bea734fa
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-12-21 20:34:27 +0530

    Add docstrings for attributes

--- acord/models/attachment.py
+++ acord/models/attachment.py
@@ -6,13 +6,43 @@ from acord.bases import Hashable
 
 class Attachment(pydantic.BaseModel, Hashable):
     id: Snowflake
-    filename: str 
+    """
+    ID of the attachment
+    """
+    filename: str
+    """
+    Name of the attached file
+    """
     description: Optional[str]
+    """
+    Description of the file
+    """
     content_type: Optional[str]
+    """
+    Media type of the attachment
+    """
     size: int
+    """
+    Size of the attachment, in bytes
+    """
     url: str
+    """
+    Source URL of the file
+    """
     proxy_url: str
+    """
+    A proxied URL of the file
+    """
     height: Optional[int]
+    """
+    Height of the file (if it's an image)
+    """
     width: Optional[int]
+    """
+    Width of the file (if its an image)
+    """
     ephemeral: Optional[bool]
+    """
+    Whether the file is ephemeral
+    """
 
