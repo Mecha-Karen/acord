4e8073dd5b1618dc3401961f4d96f75626f7c1c6
Seniatical <isaja7710@gmail.com>
2021-12-22 16:46:12 +0000

    Move annotation changes to main `__init__`

--- acord/__init__.py
+++ acord/__init__.py
@@ -3,7 +3,7 @@ ACord - An API wrapper for the discord API.
 
 Created by Mecha Karen, and is licensed under the GNU GENERAL PUBLIC LICENSE.
 """
-from typing import NamedTuple, Literal
+from typing import NamedTuple, Literal, Optional
 import logging
 
 from .bases import *
@@ -17,6 +17,12 @@ __file__ = __import__("os").path.abspath(__file__)
 __doc__ = "An API wrapper for the discord API"
 __version__ = "0.0.1a3"
 __author__ = "Mecha Karen"
+
+SelectOption.__annotations__['emoji'] = Optional[PartialEmoji]
+Button.__annotations__['emoji'] = Optional[PartialEmoji]
+TextChannel.fetch_invites.__annotations__["return"] = List[Invite]
+TextChannel.create_invite.__annotations__["return"] = List[Invite]
+Invite.__annotations__["guild"] = Optional[Guild]
 
 
 class VersionInfo(NamedTuple):
