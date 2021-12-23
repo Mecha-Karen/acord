3cb745c0476c3de528b7de9f410581390e5d64ae
Seniatical <isaja7710@gmail.com>
2021-12-23 12:22:59 +0000

    Role edit and delete method

--- acord/models/roles.py
+++ acord/models/roles.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 from typing import Any, Dict, Optional
 import pydantic
 
-from acord.bases import Hashable, Permissions
-from acord.bases.embeds import Color
-
+from acord.bases import Hashable, Permissions, Color
+from acord.core.abc import Route
+from acord.utils import _payload_dict_to_json
 from acord.models import Snowflake
 
 
@@ -49,3 +49,68 @@ class Role(pydantic.BaseModel, Hashable):
 
         id = kwargs["values"]["id"]
         return f"https://cdn.discordapp.com/role-icons/{id}/{role_icon}.png"
+
+    async def edit(self, *, reason: str = None, **data) -> Role:
+        """|coro|
+
+        Edits guild role
+
+        Parameters
+        ----------
+        name: :class:`str`
+            name for role
+        permissions: :class:`Permissions`
+            enabled/disabled permissions
+        color: :class:`Color`
+            Colour of the role
+        hoist: :class:`bool`
+            whether the role should be displayed separately in the sidebar
+        icon: :class:`File`
+            the role's icon image
+        unicode_emoji: :class:`str`
+            role's unicode emoji
+        mentionable: :class:`bool`
+            whether the role should be mentionable
+        reason: :class:`str`
+            Reason for deleting role
+        """
+        from acord.payloads import RoleEditPayload
+
+        headers = dict({'Content-Type': "application/json"})
+
+        if reason:
+            headers.update({"X-Audit-Log-Reason": reason})
+
+        payload = _payload_dict_to_json(RoleEditPayload, **data)
+
+        r = await self.conn.request(
+            Route("PATCH", path=f"/guilds/{self.guild_id}/roles/{self.id}"),
+            headers=headers,
+            data=payload
+        )
+
+        role = Role(**(await r.json()))
+        (self.conn.get_guild(self.guild_id)).roles.update({role.id: role})
+
+        return role
+
+    async def delete(self, *, reason: str = None):
+        """|coro|
+
+        Deletes role from guild
+
+        Parameters
+        ----------
+        reason: :class:`str`
+            Reason for deleting role
+        """
+        headers = dict()
+
+        if reason:
+            headers.update({'X-Audit-Log-Reason': reason})
+
+        await self.conn.request(
+            Route("DELETE", path=f"/guilds/{self.guild_id}/roles/{self.id}"),
+            headers=headers
+        )
+        (self.conn.get_guild(self.guild_id)).roles.pop(self.id)
