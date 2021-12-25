ade8e7dc6dbc7c881483491af3cd920f71047d8d
Seniatical <isaja7710@gmail.com>
2021-12-25 20:09:13 +0000

    Create callback class

--- acord/bases/enums/interactions.py
+++ acord/bases/enums/interactions.py
@@ -7,6 +7,17 @@ class InteractionType(IntEnum):
     MESSAGE_COMPONENT = 3
     APPLICATION_COMMAND_AUTOCOMPLETE = 4
 
+
+class InteractionCallback(IntEnum):
+    PONG = 1
+    CHANNEL_MESSAGE_WITH_SOURCE = 4
+    DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
+    DEFERRED_UPDATE_MESSAGE = 6
+    UPDATE_MESSAGE = 7
+    APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8
+
+
+
 class ApplicationCommandType(IntEnum):
     CHAT_INPUT = 1
     USER = 2
