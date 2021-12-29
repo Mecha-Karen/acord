6da8349ae321a8ac14681fd98865be70ef549824
Seniatical <isaja7710@gmail.com>
2021-12-29 12:19:14 +0000

    Cleanup imports

--- acord/models/__init__.py
+++ acord/models/__init__.py
@@ -9,14 +9,14 @@ from .roles import Role, RoleTags
 from .emoji import Emoji, PartialEmoji
 from .sticker import Sticker
 from .attachment import Attachment
-from .message import Message, MessageReference, WebhookMessage
 from .member import Member
-from .invite import Invite
+from .message import Message, MessageReference, WebhookMessage
 from .interaction import  (
     InteractionData,
     Interaction,
     IMessageFlags
 )
+from .invite import Invite
 from .channels import (
     Channel,
     TextChannel,
@@ -38,5 +38,3 @@ from .integrations import (
 )
 from .guild import Guild, Ban
 from .guild_template import GuildTemplate
-
-Message.update_forward_refs()
