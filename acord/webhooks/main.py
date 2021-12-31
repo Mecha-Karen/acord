9a4ff0717d35a0f95718569bfbc13427478043e2
Seniatical <isaja7710@gmail.com>
2021-12-31 19:51:07 +0000

    Add docstrings

--- acord/webhooks/main.py
+++ acord/webhooks/main.py
@@ -17,15 +17,25 @@ url_pattern = re.compile("^https:\/\/discord.com\/api\/webhooks\/[0-9]+\/[a-zA-Z
 
 class Webhook(WebhookMethods, pydantic.BaseModel):
     id: Snowflake
+    """ the id of the webhook """
     type: WebhookType
+    """ the type of the webhook """
     guild_id: Optional[Snowflake]
+    """ the guild id this webhook is for, if any """
     channel_id: Snowflake
+    """ the channel id this webhook is for, if any """
     user: Optional[User]
+    """ the user this webhook was created by (not returned when getting a webhook with its token) """
     name: str
+    """ the default name of the webhook """
     avatar: str
+    """ the default user avatar hash of the webhook """ 
     token: Optional[str]
+    """ the secure token of the webhook (returned for Incoming Webhooks) """
     application_id: Snowflake
+    """ the bot/OAuth2 application that created this webhook """
     url: Optional[str]
+    """ the url used for executing the webhook """
 
     @classmethod
     async def from_id(cls, id: Snowflake):
@@ -43,9 +53,12 @@ class Webhook(WebhookMethods, pydantic.BaseModel):
             ) as r:
                 return cls(**(await r.json()))
 
+
 class PartialWebhook(WebhookMethods):
     id: Snowflake
+    """ ID of webhook """
     token: str
+    """ Webhook token """
 
     def __init__(self, adapter = None, *, url: str = None, **data):
         if url is not None:
