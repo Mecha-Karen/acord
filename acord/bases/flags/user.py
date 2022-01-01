fe83b03b8b2c14fbf10599cffc906c0913bf8764
Seniatical <isaja7710@gmail.com>
2022-01-01 20:01:41 +0000

    Add application flags

--- acord/bases/flags/user.py
+++ acord/bases/flags/user.py
@@ -84,3 +84,14 @@ class UserFlags(Flag, metaclass=BaseFlagMeta):
     VERIFIED_BOT = 1 << 16
     VERIFIED_DEVELOPER = 1 << 17
     CERTIFIED_MODERATOR = 1 << 18
+
+
+class ApplicationFlags(Flag, metaclass=BaseFlagMeta):
+    GATEWAY_PRESENCE = 1 << 12
+    GATEWAY_PRESENCE_LIMITED = 1 << 13
+    GATEWAY_GUILD_MEMBERS = 1 << 14
+    GATEWAY_GUILD_MEMBERS_LIMITED = 1 << 15
+    VERIFICATION_PENDING_GUILD_LIMIT = 1 << 16
+    EMBEDDED = 1 << 17
+    GATEWAY_MESSAGE_CONTENT = 1 << 18
+    GATEWAY_MESSAGE_CONTENT_LIMITED = 1 << 19
