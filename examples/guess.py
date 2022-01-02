200e556fe439378b704f9f8a06417a150549a68c
Seniatical <isaja7710@gmail.com>
2022-01-02 17:20:56 +0000

    Cleanup example

--- examples/guess.py
+++ examples/guess.py
@@ -1,14 +1,15 @@
 from acord import Client, Message, Intents
 import random
+
 class MClient(Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
     async def on_ready(self):
-         print(f'------------\n{self.user.username}#{self.user.discriminator}\n------------\n{self.user.id}')
+        print(f'------------\n{self.user.username}#{self.user.discriminator}\n------------\n{self.user.id}')
+
     async def on_message(self, message: Message) -> None:
         if message.content.startswith(".guess"):
             number = random.randint(0, 20)
-            if int(message.content.split('.guess')[1]) == number:
+            _, *args = message.content.split(" ")
+            if int(args[0]) == number:
                 return await message.channel.send(content='Correct Guess')
             else:
                 return await message.channel.send(content=f'Wrong Guess. The number was {number}!')
