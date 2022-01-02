5fef197b3da35cf861cefd8383f1786d6f9e34eb
sldless <sldless@dpsfklmfk.com>
2022-01-02 11:27:45 -0500

    Add guess as an example

---
+++ examples/guess.py
@@ -0,0 +1,19 @@
+from acord import Client, Message, Intents
+import random
+class MClient(Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    async def on_ready(self):
+         print(f'------------\n{self.user.username}#{self.user.discriminator}\n------------\n{self.user.id}')
+    async def on_message(self, message: Message) -> None:
+        print(message.content)
+        if message.content.startswith(".guess"):
+            number = random.randint(0, 20)
+            if int(message.content.split('.guess')[1]) == number:
+                return await message.channel.send(content='Correct Guess')
+            else:
+                return await message.channel.send(content=f'Wrong Guess. The number was {number}!')
+            
+if __name__ == "__main__":
+    client = MClient(intents=Intents.ALL)
+    client.run("BOT TOKEN")
