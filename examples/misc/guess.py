9ce57195ccb398dfa99e50e5cad23f9afad87e79
Seniatical <isaja7710@gmail.com>
2022-01-02 20:21:33 +0000

    Move into misc folder

--- examples/misc/guess.py
+++ examples/misc/guess.py
@@ -3,7 +3,7 @@ import random
 
 class MClient(Client):
     async def on_ready(self):
-        print(f'------------\n{self.user.username}#{self.user.discriminator}\n------------\n{self.user.id}')
+        print(f'{self.user} is now online!')
 
     async def on_message(self, message: Message) -> None:
         if message.content.startswith(".guess"):
@@ -13,7 +13,8 @@ class MClient(Client):
                 return await message.channel.send(content='Correct Guess')
             else:
                 return await message.channel.send(content=f'Wrong Guess. The number was {number}!')
-          
+
+
 if __name__ == "__main__":
     client = MClient(intents=Intents.ALL)
     client.run("BOT TOKEN")
