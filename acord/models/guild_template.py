6fada6c277945aae7afd135008623849c7ef1617
Seniatical <isaja7710@gmail.com>
2022-01-07 14:38:59 +0000

    Complete template methods

--- acord/models/guild_template.py
+++ acord/models/guild_template.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import pydantic
 
 from typing import Any, Optional
-from acord.models import Snowflake, User, Guild
+from acord.core.abc import Route
+from acord.models import Snowflake, User
+from acord.payloads import TemplateCreatePayload
 
 
 class GuildTemplate(pydantic.BaseModel):
@@ -27,7 +29,7 @@ class GuildTemplate(pydantic.BaseModel):
     """ when this template was last synced to the source guild """
     source_guild_id: Snowflake
     """ the ID of the guild this template is based on """
-    serialized_source_guild: Guild
+    serialized_source_guild: Any
     """ the guild snapshot this template contains """
     is_dirty: Optional[bool]
     """ whether the template has unsynced changes """
@@ -40,3 +42,54 @@ class GuildTemplate(pydantic.BaseModel):
         creator.conn = conn
 
         return creator
+
+    @pydantic.validator("serialized_source_guild", pre=True)
+    def _get_cached_guild(cls, _, **kwargs) -> Any:
+        conn = kwargs["values"]["conn"]
+        id = kwargs["values"]["source_guild_id"]
+
+        return conn.client.get_guild(id)
+
+    async def sync(self) -> GuildTemplate:
+        """|coro|
+
+        Syncs current guild template.
+        """
+        r = await self.conn.request(
+            Route("PUT", path=f"/guilds/{self.id}/templates/{self.code}")
+        )
+
+        return GuildTemplate(conn=self.conn, **(await r.json()))
+
+    async def edit(self, **data) -> GuildTemplate:
+        """|coro|
+
+        Edits current template
+
+        Parameters
+        ----------
+        name: :class:`str`
+            name of template
+        description: :class:`str`
+            description of template
+        """
+        payload = TemplateCreatePayload(**data)
+
+        r = await self.conn.request(
+            Route("PATCH", path=f"/guilds/{self.id}/templates/{self.code}"),
+            data=payload.json(),
+            headers={"Content-Type": "application/json"}
+        )
+
+        return GuildTemplate(conn=self.conn, **(await r.json()))
+
+    async def delete(self) -> GuildTemplate:
+        """|coro|
+
+        Deletes template, returns template on success.
+        """
+        r = await self.conn.request(
+            Route("DELETE", path=f"/guilds/{self.id}/templates/{self.code}")
+        )
+
+        return GuildTemplate(**(await r.json()))
