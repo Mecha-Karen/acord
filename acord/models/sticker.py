9ff48593baf48eba1ccba6bc6d1be5646914f1ef
Seniatical <isaja7710@gmail.com>
2022-01-08 19:58:42 +0000

    Allow stickers to be created from code and be deleted

--- acord/models/sticker.py
+++ acord/models/sticker.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import pydantic
 
+from acord.core.abc import Route
 from acord.bases import Hashable
 from acord.models import Snowflake, User
 from typing import Any, Optional
@@ -33,3 +34,43 @@ class Sticker(pydantic.BaseModel, Hashable):
     """ the user that uploaded the guild sticker """
     sort_value: Optional[int]
     """ the standard sticker's sort order within its pack """
+
+    @pydantic.validator("user")
+    def _validate_user(cls, user, **kwargs):
+        if not user:
+            return
+        conn = kwargs["values"]["conn"]
+        user.conn = conn
+
+        return user
+
+    @classmethod
+    async def from_code(cls, client, sticker_id: Snowflake) -> Sticker:
+        """|coro|
+
+        Fetches sticker from API,
+        by using existing client and id
+
+        Parameters
+        ----------
+        client: :class:`Client`
+            client to fetch sticker from
+        sticker_id: :class:`Snowflake`
+            id of sticker to fetch
+        """
+        r = await client.http.request(Route("GET", path=f"/stickers/{sticker_id}"))
+        return Sticker(**(await r.json()))
+
+    async def delete(self, *, reason: str = None) -> None:
+        """|coro|
+
+        Deletes this sticker
+        """
+        headers = {}
+        if reason is not None:
+            headers["X-Audit-Log-Reason"] = reason
+        
+        await self.conn.request(
+            Route("DELETE", path=f"/guilds/{self.guild_id}/stickers/{self.id}"), 
+            headers=headers
+            )
