a5c365eca15f04d9463be4058fef1daeb0089c77
Seniatical <isaja7710@gmail.com>
2022-01-16 16:39:25 +0000

    Feat: .join shortcut

--- acord/models/channels/voice.py
+++ acord/models/channels/voice.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
-from typing import List, Optional
+from typing import Any, List, Optional
 import pydantic
 
 from acord.bases import PermissionsOverwrite
 from acord.models import Snowflake
+from acord.voice.core import VoiceWebsocket
 from .base import Channel
 
 
@@ -16,6 +17,8 @@ class VoiceRegion(pydantic.BaseModel):
 
 
 class VoiceChannel(Channel):
+    conn: Any
+
     guild_id: Snowflake
     """ ID of guild were channel is in """
     name: str
@@ -34,3 +37,34 @@ class VoiceChannel(Channel):
     """ ID of category were channel is in """
     rtc_region: Optional[str]
     """ voice region id for the voice channel, automatic when set to null """
+
+    async def join(self, self_mute: bool = False, self_deaf: bool = False) -> VoiceWebsocket:
+        """|coro|
+
+        Joins this voice channel,
+        shortcut for :class:`Client.update_voice_state`.
+
+        Parameters
+        ----------
+        self_mute: :class:`bool`
+            Whether to mute client on join
+        self_deaf: :class:`bool`
+            Whether to deafen client on join,
+            doesn't mute unless specified!
+
+        Returns
+        -------
+        Returns a :class:`VoiceWebsocket` object,
+        which can directly be passed into a reciever/player
+        """
+        await self.conn.client.update_voice_state(
+            guild_id=self.guild_id,
+            channel_id=self.id,
+            self_mute=self_mute,
+            self_deaf=self_deaf
+        )
+
+        return await self.conn.client.wait_for(
+            "voice_server_update", 
+            check=lambda vc: vc.identity()["d"]["server_id"] == self.id,
+        )
