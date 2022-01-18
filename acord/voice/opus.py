37410d9cbc44b8068703a57ac93b72bfd3471172
Seniatical <isaja7710@gmail.com>
2022-01-18 20:33:39 +0000

    Add decoder class

--- acord/voice/opus.py
+++ acord/voice/opus.py
@@ -1,4 +1,8 @@
+# Refactored code from "discord/opus.py"
+# Source: https://github.com/Rapptz/discord.py/
+
 from __future__ import annotations
+from typing import Optional
 
 import ctypes
 import ctypes.util
@@ -8,9 +12,11 @@ import array
 import sys
 import logging
 import pydantic
+import math
 from enum import Enum
 
 from acord import __file__ as file_loc
+from acord.errors import VoiceError
 
 logger = logging.getLogger(__name__)
 
@@ -146,6 +152,92 @@ class Encoder(object):
         return array.array('b', data[:ret]).tobytes()
 
 
-
 class Decoder(object):
     """Default decoder for opus"""
+    def __init__(self, *,
+        opus: ctypes.CDLL = opus, 
+        opus_config: OpusConfig = None, 
+        **kwargs
+    ) -> None:
+        self.lib = opus
+        self.config = opus_config or OpusConfig(**kwargs)
+
+        self._state = self._create_state()
+
+    def __del__(self) -> None:
+        if hasattr(self, '_state'):
+            self.lib.opus_decoder_destroy(self._state)
+            self._state = None
+
+    def _create_state(self):
+        ret = ctypes.c_int()
+        return self.lib.opus_decoder_create(
+            self.config.SAMPLING_RATE, 
+            self.config.CHANNELS, 
+            ctypes.byref(ret)
+        )
+
+    def packet_get_nb_frames(self, data: bytes) -> int:
+        """Gets the number of frames in an Opus packet"""
+        return self.lib.opus_packet_get_nb_frames(data, len(data))
+
+    def packet_get_nb_channels(self, data: bytes) -> int:
+        """Gets the number of channels in an Opus packet"""
+        return self.lib.opus_packet_get_nb_channels(data)
+
+    def packet_get_samples_per_frame(self, data: bytes) -> int:
+        return self.lib.opus_packet_get_samples_per_frame(data, self.config.SAMPLING_RATE)
+
+    def _set_gain(self, adjustment: int) -> int:
+        """Configures decoder gain adjustment.
+        Scales the decoded output by a factor specified in Q8 dB units.
+        This has a maximum range of -32768 to 32767 inclusive, and returns
+        OPUS_BAD_ARG (-1) otherwise. The default is zero indicating no adjustment.
+        This setting survives decoder reset (irrelevant for now).
+        gain = 10**x/(20.0*256)
+
+        (from opus_defines.h)
+        """
+        return self.lib.opus_decoder_ctl(self._state, CTL_SET_GAIN, adjustment)
+
+    def set_gain(self, dB: float) -> int:
+        """Sets the decoder gain in dB, from -128 to 128."""
+
+        dB_Q8 = max(-32768, min(32767, round(dB * 256))) 
+        # dB * 2^n where n is 8 (Q8)
+        return self._set_gain(dB_Q8)
+
+    def set_volume(self, mult: float) -> int:
+        """Sets the output volume as a float percent, i.e. 0.5 for 50%, 1.75 for 175%, etc."""
+        return self.set_gain(20 * math.log10(mult))
+
+    def _get_last_packet_duration(self) -> int:
+        """Gets the duration (in samples) of the last packet successfully decoded or concealed."""
+
+        ret = ctypes.c_int32()
+        self.lib.opus_decoder_ctl(self._state, CTL_LAST_PACKET_DURATION, ctypes.byref(ret))
+        return ret.value
+
+    def decode(self, data: Optional[bytes], *, fec: bool = False) -> bytes:
+        if data is None and fec:
+            raise VoiceError("Invalid arguments: FEC cannot be used with null data")
+
+        if data is None:
+            frame_size = self._get_last_packet_duration() or self.SAMPLES_PER_FRAME
+            channel_count = self.CHANNELS
+        else:
+            frames = self.packet_get_nb_frames(data)
+            channel_count = self.packet_get_nb_channels(data)
+            samples_per_frame = self.packet_get_samples_per_frame(data)
+            frame_size = frames * samples_per_frame
+
+        pcm = (ctypes.c_int16 * (frame_size * channel_count))()
+        pcm_ptr = ctypes.cast(pcm, c_int16_ptr)
+
+        ret = self.lib.opus_decode(
+            self._state, 
+            data, 
+            len(data) if data else 0, pcm_ptr, frame_size, 
+            fec)
+
+        return array.array('h', pcm[:ret * channel_count]).tobytes()
