6f8fbb0a4a1847c526fdcf06efbc5994717348f4
Seniatical <isaja7710@gmail.com>
2022-01-23 16:08:43 +0000

    Rewrite configs

--- acord/voice/opus.py
+++ acord/voice/opus.py
@@ -19,19 +19,14 @@ from asyncio import get_event_loop
 
 
 class OpusConfig(pydantic.BaseModel):
-    SAMPLING_RATE: int = 48000
+    APPLICATION: str = "audio"
+    SAMPLING_RATE: float = 48000
     CHANNELS: int = 2
-    FRAME_LENGTH: int = (20 / 1000) # in ms
-    SAMPLE_SIZE: float = struct.calcsize('h') * CHANNELS
-    SAMPLES_PER_FRAME: float = SAMPLING_RATE * FRAME_LENGTH
-    FRAME_SIZE: int = int(SAMPLES_PER_FRAME * SAMPLE_SIZE)
+    FRAME_LENGTH: int = 20  # in milliseconds
+    SAMPLE_SIZE: int = struct.calcsize('h') * CHANNELS
+    SAMPLES_PER_FRAME: float = int(SAMPLING_RATE / 1000 * FRAME_LENGTH)
+    FRAME_SIZE: float = SAMPLES_PER_FRAME * SAMPLE_SIZE
 
-    BITRATE: int = 128
-    EXPECTED_PACKET_LOSS_PERCENT: float = 0.15
-    BANDWIDTH: str = "full"
-    SIGNAL_TYPE = "auto"
-    APPLICATION = "audio"
-    SAMPLE_WIDTH = 16
 
 DEFAULT_CONFIG = OpusConfig()
 
@@ -47,26 +42,25 @@ class Encoder(OpusEncoder):
     def setup(self):
         super().set_application(self.config.APPLICATION)
         super().set_channels(self.config.CHANNELS)
-        super().set_max_bytes_per_frame(self.config.SAMPLE_SIZE)
+        super().set_max_bytes_per_frame(self.config.FRAME_SIZE)
         super().set_sampling_frequency(self.config.SAMPLING_RATE)
         # NOTE: await further changes from PyOgg for bitrate and other funcs
 
     async def encode(self, pcm: bytes) -> bytes:
-        ef_frame_size = (
-            len(pcm)
-            // self.config.SAMPLE_WIDTH
-            // self.config.CHANNELS
-        )
+        # ef_frame_size = (
+        #     len(pcm)
+        #     // 2    # Sample Width
+        #     // self.config.CHANNELS
+        # )
 
         # if ef_frame_size < self.config.FRAME_SIZE:
             # If frame size is lower then desired config
-            # Pad packet with silence
-        #     pcm += (
-        #         b"\x00"
-        #         * ((self.config.FRAME_SIZE - ef_frame_size)
-        #             * self.config.SAMPLE_WIDTH
-        #             * self.config.CHANNELS)
-        #     )
+            # Pad end of packet with silence
+            # This should only be applicable at the end of audio files
+            # Which is were you may notice that silence
+        #    pcm += (b"\x00"
+        #            * (self.config.FRAME_SIZE - ef_frame_size)
+        #    )
 
         return await self.loop.run_in_executor(
             None, super().encode, pcm
