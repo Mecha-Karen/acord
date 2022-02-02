5dd19963c47806909c8905d070bdb3404853ad63
Seniatical <isaja7710@gmail.com>
2022-02-02 12:29:15 +0000

    Feat: .delete method

--- acord/ext/application_commands/base.py
+++ acord/ext/application_commands/base.py
@@ -5,11 +5,14 @@ import pydantic
 from acord.models import (
     Snowflake,
 )
+from acord.core.abc import Route
 from .types import ApplicationCommandType
 from .option import SlashOption
 
 
 class ApplicationCommand(pydantic.BaseModel):
+    conn: Any
+
     application_id: Snowflake
     """ unique id of the parent application """
     id: Snowflake
@@ -27,7 +30,21 @@ class ApplicationCommand(pydantic.BaseModel):
     """ guild id of the command, if not global """
     default_permission: Optional[bool] = True
     """ whether the command is enabled by default when the app is added to a guild """
+    options: Optional[List[SlashOption]] = []
+    """ Options for a slash command """
 
+    async def delete(self) -> None:
+        """|coro|
 
-class SlashExtended(ApplicationCommand):
-    options: List[SlashOption]
+        Deletes this command globally,
+        if ``guild_id`` is not ``None``,
+        the command is deleted for that guild
+        """
+        if self.guild_id is not None:
+            path = f"/applications/{self.application_id}/guilds/{self.guild_id}/commands/{self.id}"
+        else:
+            path = f"/applications/{self.application_id}/commands/{self.id}"
+
+        await self.conn.request(
+            Route("DELETE", path=path, bucket=dict(guild_id=self.guild_id))
+        )
