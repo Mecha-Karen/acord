9416f817bc1e017b116e7b832cb799577ae5a4c0
Seniatical <isaja7710@gmail.com>
2022-02-05 12:20:53 +0000

    Add proper snowflake object and __all__

--- acord/models/__init__.py
+++ acord/models/__init__.py
@@ -1,7 +1,64 @@
-from typing import TypeVar
+from __future__ import annotations
 
-Snowflake = TypeVar("Snowflake", bound=int)
-# Simple snowflake object
+from typing import final
+import datetime
+from acord.core.abc import DISCORD_EPOCH
+
+
+__all__ = (
+    "Snowflake", 
+    "PartialChannel", "PartialEmoji",
+    "User",
+    "Application",
+    "Role", "RoleTags",
+    "Emoji",
+    "Sticker",
+    "Attachment",
+    "Member",
+    "GuildScheduledEvent", "ScheduledEventUser", "ScheduledEventMetaData",
+    "Message", "MessageReference", "WebhookMessage",
+    "InteractionSlashOption", "InteractionData", "Interaction", "IMessageFlags",
+    "Invite",
+    "Channel", "TextChannel", "Stage", "Thread", "ThreadMeta", "ThreadMember",
+    "VoiceChannel", "VoiceRegion", "CategoryChannel", "DMChannel", "GroupDMChannel",
+    "IntegrationExpBehaviour", "IntegrationAccount", "IntegrationApplication", "PartialIntegration",
+    "Integration", "AuditLogChange", "AuditLogEntryInfo", "AuditLogEntry", "AuditLog",
+    "GuildTemplate", "Guild", "Ban", "GuildWidget", "GuildWidgetImageStyle", "WelcomeChannel",
+    "WelcomeScreen",
+)
+
+
+@final
+class Snowflake(int):
+    """A concrete representation of a unique id for a discord model
+
+    This object is a :class:`int` and only contains a few additional properties.
+    """
+    __slots__ = ()
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        """ When object was created """
+        epoch = (self >> 22) / 1000
+        epoch += DISCORD_EPOCH
+
+        return datetime.datetime.fromtimestamp(epoch, datetime.timezone.utc)
+
+    @property
+    def internal_worker_id(self) -> int:
+        """ ID of the worker that created snowflake. """
+        return (self & 0x3E0_000) >> 17
+
+    @property
+    def internal_process_id(self) -> int:
+        """ ID of the process that created snowflake. """
+        return (self & 0x1F_000) >> 12
+
+    @property
+    def increment(self) -> int:
+        """ Increment of when this object was made. """
+        return self & 0xFFF
+
 
 from .partials import PartialChannel, PartialEmoji
 from .user import User
@@ -46,4 +103,9 @@ from .integrations import (
 )
 from .audit_logs import AuditLogChange, AuditLogEntryInfo, AuditLogEntry, AuditLog
 from .guild_template import GuildTemplate
-from .guild import Guild, Ban
+from .guild import (
+    Guild, Ban, GuildWidget,
+    GuildWidgetImageStyle,
+    WelcomeChannel,
+    WelcomeScreen,
+)
