79f2ad69ebb4e1269921cfa1981db6eae9f5d10f
Seniatical <isaja7710@gmail.com>
2022-02-06 17:28:17 +0000

    Add other method for Client

--- acord/client/client.pyi
+++ acord/client/client.pyi
@@ -1,9 +1,20 @@
 import asyncio
-from re import A, S
-from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Union, overload
+from typing import Any, AsyncGenerator, Callable, Dict, List, Literal, Optional, Tuple, Union, overload
 from acord.bases import _C
 
-from acord import User, Presence, Snowflake, Stage, StagePrivacyLevel, Guild
+from acord import (
+    User, 
+    Presence, 
+    Snowflake, 
+    Stage, 
+    StagePrivacyLevel,
+    Guild,
+    UDAppCommand, 
+    ApplicationCommand,
+    VoiceConnection,
+    Message,
+    Channel
+)
 
 class Client(object):
     INTERNAL_STORAGE: Dict[str, Any]
@@ -16,9 +27,14 @@ class Client(object):
     gateway_version: Optional[Union[str, int]]
     user: Optional[User]
     awaiting_voice_connections: Dict[str, str]
+    voice_connections: Dict[int, VoiceConnection]
+    acked_at: float
+    latency: float
 
     # Properties and what not
     guilds: List[Guild]
+
+    # Methods
     @overload
     def bind_token(self, token: str) -> None: ...
     @overload
@@ -83,6 +99,69 @@ class Client(object):
         privacy_level: StagePrivacyLevel,
         reason: str,
     ) -> Stage: ...
+    @overload
+    def register_application_command(
+        self,
+        command: UDAppCommand
+    ) -> None: ...
+    @overload
+    def register_application_command(
+        self,
+        command: UDAppCommand,
+        *,
+        guild_ids: Union[List[int], None] = None,
+        extend: bool = True,
+    ) -> None: ...
+    @overload
+    async def create_application_command(
+        command: UDAppCommand
+    ) -> Union[ApplicationCommand, List[ApplicationCommand]]: ...
+    @overload
+    async def create_application_command(
+        self,
+        command: UDAppCommand,
+        *,
+        guild_ids: Union[List[int], None] = None,
+        extend: bool = True,
+    ) -> Union[ApplicationCommand, List[ApplicationCommand]]: ...
+    @overload
+    async def bulk_update_global_app_commands(self, commands: List[UDAppCommand]) -> None: ...
+    @overload
+    async def bulk_update_guild_app_commands(
+        self,
+        guild_id: Snowflake,
+        commands: List[UDAppCommand],
+    ) -> None: ...
+    @overload
+    async def disconnect(self): ...
+    @overload
+    def get_message(self, channel_id: int, message_id: int) -> Optional[Message]: ...
+    @overload
+    def get_user(self, user_id: int) -> Optional[User]: ...
+    @overload
+    def get_guild(self, guild_id: int) -> Optional[Guild]: ...
+    @overload
+    def get_channel(self, channel_id: int) -> Optional[Channel]: ...
+    @overload
+    async def fetch_user(self, user_id: int) -> Optional[User]: ...
+    @overload
+    async def fetch_channel(self, channel_id: int) -> Optional[Channel]: ...
+    @overload
+    async def fetch_message(
+        self, channel_id: int, message_id: int
+    ) -> Optional[Message]: ...
+    @overload
+    async def fetch_guild(self, guild_id: int) -> Optional[Guild]: ...
+    @overload
+    async def fetch_guild(
+        self, guild_id: int, *, with_counts: bool = False
+    ) -> Optional[Guild]: ...
+    @overload
+    async def fetch_glob_app_commands(self) -> AsyncGenerator[ApplicationCommand, None]: ...
+    @overload
+    async def fetch_glob_app_command(self, command_id: Snowflake) -> ApplicationCommand: ...
+    @overload
+    async def gof_channel(self, channel_id: int) -> Optional[Channel]: ...
     @overload
     def run(self, token: str) -> None: ...
     @overload
