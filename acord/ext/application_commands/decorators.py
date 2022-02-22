5ffa327e8301ca1a94b97b7f7af3c8be7debbfe7
Seniatical <isaja7710@gmail.com>
2022-02-22 20:31:03 +0000

    Add autocomplete decorator

--- acord/ext/application_commands/decorators.py
+++ acord/ext/application_commands/decorators.py
@@ -1,8 +1,42 @@
 from __future__ import annotations
 from typing import Any, Callable, Coroutine
 
-import inspect
 from .slash import SlashBase
+
+
+def autocomplete(*options: str,
+    dev_handle: bool = False,
+    cls: SlashBase = None,
+    on_error: Callable[..., Coroutine[Any, Any, Any]] = None,
+    ) -> Callable[..., Coroutine[Any, Any, Any]]:
+        """Decorator for adding handlers for autocompleting parameters
+
+        Parameters
+        ----------
+        options: :class:`str`
+            Names of option to handle auto completes for,
+            use ``*`` to handle any option
+        dev_handle: :class:`bool`
+            Whether this field will be responded to within the class
+        cls: :class:`SlashBase`
+            Your command object which this autocomplete should be assigned to,
+            this is not needed unless your adding them to commands generated via decorators.
+        on_error: Callable[..., Coroutine[Any, Any, Any]]
+            A coro which will be called when an error occurs
+        """
+        def inner(callback: Callable[..., Coroutine[Any, Any, Any]]):
+            setattr(
+                callback,
+                "__autocomplete__",
+                (options, dev_handle, on_error)
+            )
+
+            if cls is not None:
+                setattr(cls, callback.__qualname__, callback)
+
+            return callback
+
+        return inner
 
 
 def slash_command(**kwargs):
