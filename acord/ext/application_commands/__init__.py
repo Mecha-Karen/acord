4a661e632faa8ed3f300d31126a69f31b63e6f57
Seniatical <isaja7710@gmail.com>
2022-02-22 20:30:48 +0000

    Import decorators

--- acord/ext/application_commands/__init__.py
+++ acord/ext/application_commands/__init__.py
@@ -1,5 +1,6 @@
 from .types import ApplicationCommandType, ApplicationCommandOptionType
 from .base import UDAppCommand, ApplicationCommand
-from .option import GenericApplicationOption, SlashOption
+from .option import GenericApplicationOption, SlashOption, AutoCompleteChoice
 from .slash import SlashBase
 from .model_based import GenericModelCommand, UserCommand, MessageCommand
+from .decorators import autocomplete, slash_command
