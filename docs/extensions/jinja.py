b71299e9b4a781a5cb05ba73de5156132ea719ee
Seniatical <isaja7710@gmail.com>
2022-02-23 18:58:17 +0000

    Few functions for handling inherited objects

--- docs/extensions/jinja.py
+++ docs/extensions/jinja.py
@@ -3,8 +3,30 @@ Allows us to use jinja within our .rst files
 
 We can use this to our advantage as generated rst is processed afterwards
 """
+import inspect
 from sphinx.application import Sphinx
 import acord
+
+SHARED_PROPERTIES = ['Config', 'construct', 'copy', 'dict', 'from_orm', 
+                     'json', 'parse_file', 'parse_obj', 'parse_raw', 'schema', 
+                     'schema_json', 'update_forward_refs', 'validate']
+
+
+def filter_properties(cls):
+    elements = dir(cls)
+    filtered = []
+
+    for element in elements:
+        if element.startswith("_") or element in SHARED_PROPERTIES:
+            continue
+        filtered.append(element)
+
+    return filtered
+
+def is_function(cls):
+    if inspect.isfunction(cls):
+        return True
+    return True
 
 
 def worker(app: Sphinx, docname: str, source: list):
@@ -18,7 +40,8 @@ def worker(app: Sphinx, docname: str, source: list):
             "getattr": getattr,
             "dir": dir,
             "disallow": ["final", ],
-            "LINE_SEP": "\n",
+            "filter_properties": filter_properties,
+            "is_function": is_function
         }
     )
 
