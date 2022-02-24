7cf1efcefa519850178fd37c0500474ec33be73d
Seniatical <isaja7710@gmail.com>
2022-02-24 19:19:52 +0000

    Gateway handling done by Shard not HTTPClient

--- acord/core/http.py
+++ acord/core/http.py
@@ -18,7 +18,6 @@ except ImportError:
     )
 
 import asyncio
-import time
 import typing
 import aiohttp
 import acord
@@ -53,8 +52,6 @@ class HTTPClient(object):
         A pre-existing loop for aiohttp to run of, defaults to ``asyncio.get_event_loop()``
     reconnect: :class:`bool`
         Attempt to reconnect to gateway if failed, If set to a integer, it will re-attempt n times.
-    wsTimeout: :class:`~aiohttp.ClientTimeout`
-        Custom timeout configuration for
     **payloadData: :class:`dict`
         A dictionary of payload data to be sent with any request
 
@@ -69,27 +66,13 @@ class HTTPClient(object):
         token: str = None,
         connecter: typing.Optional[aiohttp.BaseConnector] = None,
         wsTimeout: aiohttp.ClientTimeout = aiohttp.ClientTimeout(60, connect=None),
-        proxy: typing.Optional[str] = None,
-        proxy_auth: typing.Optional[aiohttp.BasicAuth] = None,
         loop: typing.Optional[asyncio.AbstractEventLoop] = asyncio.get_event_loop(),
-        unsync_clock: bool = True,
     ) -> None:
         self.client = client
         self.token = token
         self.loop = loop
         self.wsTimeout = wsTimeout
         self.connector = connecter
-
-        self._ws_connected = False
-        self.proxy = proxy
-        self.proxy_auth = proxy_auth
-        self.use_clock = not unsync_clock
-
-        self._browser = "acord"
-        self._device = "acord"
-        self._os = sys.platform
-        self._referrer = None
-        self._referring_domain = None
 
         user_agent = "ACord - https://github.com/Mecha-Karen/ACord {0} Python{1[0]}.{1[1]} aiohttp/{2}"
         self.user_agent = user_agent.format(
@@ -103,42 +86,17 @@ class HTTPClient(object):
             self._lock = asyncio.Lock()
         self.trappedBuckets = dict()
 
-    def getIdentityPacket(self, intents=0, large_threshold=250, presence: dict = {}):
-        if hasattr(intents, "value"):
-            # enum.Flag cleanup
-            intents = intents.value
-
-        return {
-            "op": gateway.IDENTIFY,
-            "d": {
-                "token": self.token,
-                "intents": intents,
-                "properties": {
-                    "$os": self._os,
-                    "$browser": self._browser,
-                    "$device": self._device,
-                    "$referrer": self._referrer,
-                    "$referring_domain": self._referring_domain,
-                },
-                "compress": self.compress,
-                "large_threshold": large_threshold,
-                "presence": presence
-            },
-        }
-
-    async def login(self, *, token: str) -> None:
+    async def login(self, *, token: str) -> dict:
         """Define a session for the http client to use."""
         self._session = aiohttp.ClientSession(connector=self.connector)
-        ot = self.token
 
         self.token = token
 
         try:
-            logger.debug("Attempting to log in")
             data = await self.request(abc.Route("GET", path="/users/@me"))
-            logger.info("Sucessfully logged in")
+            
+            logger.info("Client has sucessfully logged in")
         except HTTPException as exc:
-            self.token = ot
             logger.error("Failed to login to discord, improper token passed")
             raise GatewayConnectionRefused("Invalid or Improper token passed") from exc
 
@@ -149,97 +107,11 @@ class HTTPClient(object):
         logger.info("Logging user out")
         await self.request(abc.Route("POST", path="/auth/logout"))
 
-    async def _fetchGatewayURL(self, token):
-        uri = abc.buildURL("gateway", "bot")
+    async def fetch_gateway(self):
+        route = abc.Route("GET", path=f"/gateway/bot")
 
-        async with self._session.get(
-            uri, headers={"Authorization": f"Bot {token}"}
-        ) as resp:
-            data = await resp.json()
-
-            return data
-
-    async def decodeResponse(self, resp):
-        if isinstance(resp, aiohttp.WSMessage):
-            data = resp.data
-        elif isinstance(resp, aiohttp.ClientResponse):
-            data = await resp.text()
-        else:
-            raise TypeError("Invalid response provided")
-
-        if isinstance(data, bytes) or getattr(self, "compress", False):
-            data = decompressResponse(data)
-
-        if not data.startswith("{"):
-            data = ETF(data)
-        else:
-            data = JSON(data)
-
-        return data
-
-    async def _connect(
-        self, token: str, *, encoding, compress=0, **identityPacketKwargs
-    ) -> None:
-        if not getattr(self, "_session", False):
-            logger.warn(
-                "Session not defined, user not logged in. Called login manually"
-            )
-            await self.login(token=(token or self.token))
-
-        self.encoding = encoding
-        self.compress = compress
-
-        respData = await self._fetchGatewayURL(token)
-        GATEWAY_WEBHOOK_URL = respData["url"]
-
-        GATEWAY_WEBHOOK_URL += f"?v={abc.API_VERSION}"
-        GATEWAY_WEBHOOK_URL += f"&encoding={encoding.lower()}"
-
-        if compress:
-            GATEWAY_WEBHOOK_URL += "&compress=zlib-stream"
-
-        logger.info("Generated websocket url: %s" % GATEWAY_WEBHOOK_URL)
-
-        kwargs = {
-            "proxy_auth": self.proxy_auth,
-            "proxy": self.proxy,
-            "max_msg_size": 0,
-            "timeout": self.wsTimeout,
-            "autoclose": False,
-            "headers": {
-                "User-Agent": self.user_agent,
-            },
-            "compress": compress,
-        }
-
-        self._state = (GATEWAY_WEBHOOK_URL, kwargs)
-
-        ws = await self._session.ws_connect(GATEWAY_WEBHOOK_URL, **kwargs)
-
-        helloRecv = await ws.receive()
-        data = await self.decodeResponse(helloRecv)
-
-        self._ws_connected = True
-
-        self.ws = ws
-
-        # Keep links for API_OBJECTS
-        self.ws.client = self
-
-        self._keep_alive = KeepAlive(
-            self.client,
-            self.getIdentityPacket(**identityPacketKwargs), 
-            ws, data
-        )
-        self._keep_alive.start()
-
-        return ws
-
-    async def disconnect(self) -> None:
-        logger.info("Disconnected from discord, closing WS & session")
-
-        await self.ws.close(code=4000)
-        await self._session.close()
+        r = await self.request(route)
+        return await r.json()
 
     async def request(
         self,
