2862849557f4ba75d44eeda0009460d8935430e6
Seniatical <isaja7710@gmail.com>
2022-02-26 12:29:53 +0000

    Fix errors with ratelimiter

--- acord/client/ratelimiter.py
+++ acord/client/ratelimiter.py
@@ -38,13 +38,11 @@ class GatewayRatelimiter(ABC, BaseModel):
     """ A tuple containing the limit of requests allowed to be made, (rate, per) """
     current_requests: Dict[int, int] = {}
     """ Mapping of number of requests each ratelimit_key has made """
-    blocked: bool = False
-    """ Whether to block any requests till reset """
     refreshed_at: int = 0
     """ Timestamp of when the ratelimiter was last refreshed at """
     tasks: Dict[str, Any] = {}
     """ A mapping of generated tasks """
-    shards: Dict[int, Set[Any]]
+    shards: Dict[int, Set[Any]] = {}
     """ Shards this ratelimiter is attached to,
     a mapping of RATELIMIT_KEY and Set[Shard]
     """
@@ -111,7 +109,7 @@ class GatewayRatelimiter(ABC, BaseModel):
         """
 
     @abstractmethod
-    def add_shard(self, shard) -> None:
+    def add_shard(self, shard, overwrite: bool = False) -> None:
         """Adds a :class:`Shard` to the ratelimiter
         
         Parameters
@@ -128,3 +126,99 @@ class DefaultGatewayRatelimiter(GatewayRatelimiter):
         (120 - 3),
         60
     )
+
+    def __init__(self, **kwds) -> None:
+        super().__init__(**kwds)
+
+        loop.create_task(self._reset_requests_task())
+
+    def increment(self, key: int, lock_if_exceed: bool = False) -> None:
+        if not key in self.current_requests:
+            raise RuntimeError("Unknown ratelimit key provided")
+
+        self.current_requests[key] += 1
+
+        if lock_if_exceed and self.exceeded(key):
+            self.lock(key)
+
+    def exceeded(self, key: int) -> bool:
+        cur = self.current_requests.get(key)
+
+        if cur is None:
+            raise RuntimeError("Unknown ratelimit key provided")
+
+        if self.tasks.get(key, {}).get("lock") is not None or (
+            cur >= self.max_requests[0]
+        ):
+            return True
+        return False
+
+    def lock(self, key: int) -> None:
+        tasks = self.tasks.get(key, {})
+
+        if tasks.get("lock"):
+            raise RuntimeError("This key has already been locked")
+
+        async def future():
+            cur = time()
+            d = cur - self.refreshed_at
+
+            await sleep(d)
+
+            self.tasks[key].pop("lock")
+
+        task = loop.create_task(future())
+        
+        if not tasks:
+            self.task[key] = {"lock": task}
+        else:
+            self.tasks[key]["lock"] = task
+
+    async def hold_until_reset(self, key: int) -> None:
+        tasks = self.tasks.get(key)
+
+        if not tasks:
+            return
+        if lock_task := tasks.get("lock"):
+            await lock_task
+        else:
+            futs = self.tasks.get("futures")
+            
+            if futs:
+                await futs[0]
+            else:
+                # Spawn and wait
+                fut = loop.create_future()
+                
+                if futs is None:
+                    self.tasks["futures"] = [fut]
+                else:
+                    self.tasks["futures"].append(fut)
+
+                await wait_for(fut, timeout=None)
+
+    def add_shard(self, shard, overwrite: bool = False) -> None:
+        key = shard.ratelimit_key
+
+        if (shards := self.shards.get(key)):
+            if shard in shards and overwrite:
+                shards.update((shard, ))
+                self.shards[key] = shards
+        else:
+            self.shards[key] = {shard}
+
+        if key not in self.current_requests:
+            self.current_requests[key] = 0
+
+    async def _reset_requests_task(self):
+        while True:
+            await sleep(self.max_requests[1])
+
+            for key in self.max_requests:
+                self.max_requests[key] = 0
+
+            if futs := self.tasks.get("futures"):
+                for future in futs:
+                    future.set_result(1)
+
+                    self.tasks["futures"].remove(future)
