734706e8947379562148ba9b9536bc6f97e22cf7
Seniatical <isaja7710@gmail.com>
2022-03-04 21:10:25 +0000

    Cleanup streaming func

--- acord/bases/presence.py
+++ acord/bases/presence.py
@@ -111,6 +111,12 @@ def streaming(
     name: str, url: str, *, status: StatusType = StatusType.online
 ) -> Presence:
     return Presence(
-        activities=[Activity(name=name, type=ActivityType.STREAMING, url=url)],
+        activities=[
+            Activity(
+                name=name, 
+                type=ActivityType.STREAMING, 
+                url=url
+            )
+        ],
         status=status,
     )
